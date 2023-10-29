993f9cf0cdfb4b10c22010fb4033650b8cb98231
abdoulahadi <88839248+abdoulahadi@users.noreply.github.com>
2023-10-29 14:58:49 +0000

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,6 +2,6 @@ class Settings:
     def __init__(self):
         self.app_name = "FastAPI CRF-XelKoomIA"
         self.debug = True
-        self.crf_model_path = "C:/Users/pc/Documents/PROJET/IA-PAS/ner-application/pas-backend/app/crf_model/model/wolof_crf_model.crfsuite"
+        self.crf_model_path = "app/crf_model/model/wolof_crf_model.crfsuite"
 
 settings = Settings()
