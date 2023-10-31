2065ae513183e09e2d20e0c59e7e251af3ea0d33
abdoulahadi <88839248+abdoulahadi@users.noreply.github.com>
2023-10-31 05:24:38 +0000

    Update predict.py

--- app/crf_model/predict.py
+++ app/crf_model/predict.py
@@ -2,53 +2,53 @@ import re
 from .pre_processing import word2features
 from .tagger import initialize_tagger
 
-def predict_annotations(sentence):
+
+
+def predict_annotations(text):
     tagger = initialize_tagger()
+    # Découpez le texte en phrases en utilisant une expression régulière
+    sentences = re.split(r'(?<=[.!?])\s', text)
 
-    # Divisez la phrase en mots ou en ponctuation en utilisant une expression régulière
-    words = re.findall(r'\b\w+\b|[.,!?;:]', sentence)
 
-    # Extraction des caractéristiques de la phrase
-    input_sentence = [(word, "") for word in words]
-
-    # Utilisez le modèle (tagger) pour annoter la phrase
-    predicted_labels = tagger.tag([word2features(input_sentence, i) for i in range(len(input_sentence))])
-
-    # Créez une liste pour stocker les entités nommées
+    # Ajout de la déclaration ici
     entities = []
     start = 0
-    sentences = ""
+    sentences_text = ""
+    for sentence in sentences:
+        words = re.findall(r'\b\w+\b|[.,!?;:]', sentence)
+        input_sentence = [(word, "") for word in words]
+        predicted_labels = tagger.tag([word2features(input_sentence, i) for i in range(len(input_sentence))])
 
-    # Parcourez les mots et leurs étiquettes prédites
-    for i in range(len(input_sentence)):
-        word, label = input_sentence[i], predicted_labels[i]
 
-        # Ajoutez le mot à la phrase en cours
-        sentences += word[0] + " "
+        for i in range(len(input_sentence)):
+            word, label = input_sentence[i], predicted_labels[i]
 
-        if label.startswith('B-'):
-            tag = label[2:]  # Supprimez le préfixe 'B-'
+            # Ajoutez le mot à la phrase en cours
+            sentences_text += word[0] + " "
 
-            # Créez une entité nommée
-            entity = {
-                "text": word[0],
-                "label": tag,
-                "start": len(sentences) - 1 - len(word[0]),
-                "end": len(sentences) - 1
-            }
-            j = i + 1
+            if label.startswith('B-'):
+                tag = label[2:]  # Supprimez le préfixe 'B-'
 
-            # Recherchez les mots suivants avec des étiquettes I-
-            while j < len(input_sentence) and predicted_labels[j].startswith('I-' + tag):
-                entity["text"] += " " + input_sentence[j][0]
-                entity["end"] += len(input_sentence[j][0]) + 1  # Ajoutez 1 pour l'espace
-                start = entity["end"] + 1
-                j += 1
+                # Créez une entité nommée
+                entity = {
+                    "text": word[0],
+                    "label": tag,
+                    "start": len(sentences_text) - 1 - len(word[0]),
+                    "end": len(sentences_text) - 1
+                }
+                j = i + 1
 
-            # Ajoutez l'entité nommée à la liste d'entités
-            entities.append(entity)
+                # Recherchez les mots suivants avec des étiquettes I-
+                while j < len(input_sentence) and predicted_labels[j].startswith('I-' + tag):
+                    entity["text"] += " " + input_sentence[j][0]
+                    entity["end"] += len(input_sentence[j][0]) + 1  # Ajoutez 1 pour l'espace
+                    start = entity["end"] + 1
+                    j += 1
 
-        elif label == "O":
-            start += len(word) + 1  # Ajoutez 1 pour l'espace
+                # Ajoutez l'entité nommée à la liste d'entités
+                entities.append(entity)
 
-    return {"entities": entities, "sentences": sentences}
+            elif label == "O":
+                start += len(word[0]) + 1  # Ajoutez 1 pour l'espace
+
+    return {"entities": entities, "sentences": sentences_text}
